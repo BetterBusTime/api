430631b867d0b44baa883a7203d1c2bce27d9e07
fahali <fahim.ali.204@gmail.com>
2021-02-26 19:13:20 -0500

    deploy stage 1.3

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const stops = require("./controllers/stops");
 
 const app = express();
 
-// TODO only allow the BBT front end
 app.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Origin",
-        process.env.NODE_ENV === "production" ? "*" : "http://localhost:3000"
+        process.env.NODE_ENV === "production"
+            ? "https://betterbustime.herokuapp.com"
+            : "http://localhost:3000"
     );
     next();
 });
