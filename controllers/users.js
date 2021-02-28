56fc1b84c35f2e24e6f39020e72b1ee89decf0c1
fahali <fahim.ali.204@gmail.com>
2021-02-28 14:50:44 -0500

    login

--- controllers/users.js
+++ controllers/users.js
@@ -7,11 +7,19 @@ const router = express.Router();
 
 // check if user already exists
 const validateUniqueUsername = (req, res, next) => {
-    User.countDocuments({ username: req.body.username }, (error, count) => {
-        if (error) res.status(500).json({ data: "Internal server error." });
-        else if (count > 0) {
-            res.status(400).json({ data: "This username already exists." });
-        } else next();
+    User.countDocuments({ username: req.body.username }, (err, count) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ data: "Internal server error." });
+        }
+
+        if (count > 0) {
+            return res
+                .status(400)
+                .json({ data: "This username already exists." });
+        }
+
+        next();
     });
 };
 
@@ -20,17 +28,43 @@ router.post("/register", validateUniqueUsername, async (req, res) => {
     const hash = await bcrypt.hash(req.body.password, salt);
     const user = new User({ username: req.body.username, password: hash });
 
-    user.save((error, newUser) => {
-        if (error) {
-            res.status(500).json({ data: "Internal server error." });
-        } else {
-            res.status(201).json({ data: generateNewToken(newUser.username) });
+    user.save((err, newUser) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ data: "Internal server error." });
         }
+
+        res.status(201).json({ data: generateNewToken(newUser.username) });
     });
 });
 
 router.post("/login", (req, res) => {
-    // login route
+    User.findOne({ username: req.body.username }, async (err, user) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ data: "Internal server error." });
+        }
+
+        if (user) {
+            const valid = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+
+            if (valid) {
+                return res
+                    .status(200)
+                    .json({ data: generateNewToken(user.username) });
+            }
+
+            return res.status(401).json({
+                data:
+                    "Invalid username and/or password. Check your credentials and try again."
+            });
+        }
+
+        res.status(401).json({ data: "This user does not exist." });
+    });
 });
 
 // TODO make longer expiration time after testing authentication
